38595d07b142830fd9d899e043ea2db621970d19
vmatekole <victor@matekole.com>
2017-08-08 11:46:28 +0200

    support lightwallet

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ for buddy in $(cat /iri/conf/neighbors); do
 done
 neighbors=${neighbors::-1}
 
-exec java -XX:+DisableAttachMechanism -Xmx4g -Xms4g -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri-1.2.4.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
+exec java -XX:+DisableAttachMechanism -Xmx4g -Xms4g -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri-1.2.4.jar -r 14265 -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "removeNeighbors, addNeighbors, interruptAttachingToTangle, attachToTangle, getNeighbors" --send-limit 100.0 "$@"
